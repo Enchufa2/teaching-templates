e6635fd73dbd020b5183c5df75f074eedf9dd4b5
Iñaki Úcar <iucar@fedoraproject.org>
2024-11-21 13:25:02 +0100

    add command to install fira

--- dependencies.R
+++ dependencies.R
@@ -10,3 +10,4 @@ deps <- Filter(function(x) !requireNamespace(x, quietly=TRUE), deps)
 
 install.packages(deps, repos="https://cloud.r-project.org")
 # tinytex::install_tinytex()
+# tinytex::tlmgr_install("fira")
