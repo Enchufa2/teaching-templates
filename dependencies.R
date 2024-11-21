1db3f975c92a6d22c666295aff327159dec029e1
Iñaki Úcar <iucar@fedoraproject.org>
2024-11-21 12:24:30 +0100

    add dependencies file

---
+++ dependencies.R
@@ -0,0 +1,12 @@
+deps <- c(
+  "binb",
+  "calendar",
+  "dplyr",
+  "tufte",
+  "writexl"
+)
+
+deps <- Filter(function(x) !requireNamespace(x, quietly=TRUE), deps)
+
+install.packages(deps, repos="https://cloud.r-project.org")
+# tinytex::install_tinytex()
